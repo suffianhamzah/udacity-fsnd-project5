870097b6c35e497a2ce8f2a0393a508af75bed58
Suffian Hamzah <suffianhamzah@gmail.com>
2017-08-13 22:03:57 -0700

    Created list, now make it clickable and open marker

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const CENTER_LOCATION = '';
+var locations = [
+{ name: 'Yellowstone National Park',
+  state: 'WY',
+  country: 'USA',
+  lat: '44.427895',
+  lng: '-110.588379'
+},
+{ name: 'Rothrock State Forest',
+  state: 'PA',
+  country: 'USA',
+  lat: '40.720585',
+  lng: '-77.826965'
+},
+{ name: 'Yosemite National Park',
+  state: 'CA',
+  country: 'USA',
+  lat: '37.865101',
+  lng: '-119.538330'
+}]
+
+const mapItem = function mapDataModel(data) {
+  this.name = data.name;
+  this.lat = data.lat;
+  this.lng = data.lng;
+  this.active = ko.observable(false);
+
+}
+
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+
+  var uluru = {lat: 37.865101, lng: -119.538330};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 7,
     center: uluru
   });
   var marker = new google.maps.Marker({
@@ -10,3 +38,16 @@ function initMap() {
     map: map
   });
 }
+
+var ViewModel= {
+  locations:locations,
+  setActive: function() {
+
+  }
+}
+
+ko.applyBindings(ViewModel);
+NPS_API = '75861333-AF8F-47CE-85F3-8FAB0209987D';
+NPS_BASE_URL = 'https://developer.nps.gov/api/v0';
+
+
