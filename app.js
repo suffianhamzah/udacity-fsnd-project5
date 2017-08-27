ac173b50842ef102340d7e043fac350833c92192
Suffian Hamzah <suffianhamzah@gmail.com>
2017-08-26 20:49:57 -0700

    Added yelp API, and yelp information to infoWindow

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ const mapItem = function mapDataModel(data) {
 
 var map, center, largeInfoWindow;
 function initialize() {
-  yelp_request_2('food');
-  request('Sunnyvale', initMap);
+  yelp_request_2('food', initMap);
 }
 function initMap(events) {
   var markers = [];
@@ -36,7 +35,7 @@ function initMap(events) {
   ko.applyBindings(viewModel);
   console.log(events[0])
   events.forEach(function(location) {
-    bounds.extend( new google.maps.LatLng(parseFloat(location.venue.latitude), parseFloat(location.venue.longitude)));
+    bounds.extend( new google.maps.LatLng(location.coordinates.latitude, location.coordinates.longitude));
   });
 
   /*
@@ -46,16 +45,19 @@ function initMap(events) {
   */
   center = bounds.getCenter();
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
+    zoom: 15,
     center: center
   });
 
   var defaultIcon = makeMarkerIcon('0091ff');
+  var highlightedIcon = makeMarkerIcon('FFFF24');
   var locations = events;
-  largeInfoWindow = new google.maps.InfoWindow();
+  largeInfoWindow = new google.maps.InfoWindow({
+    maxWidth: 375
+  });
   largeInfoWindow.addListener('closeclick', function() {
     largeInfoWindow.marker = null;
-    map.setZoom(10);
+    map.setZoom(15);
     map.setCenter(center);
     viewModel.currentLocationIndex(null);
   });
@@ -63,21 +65,22 @@ function initMap(events) {
   viewModel.infoWindow = largeInfoWindow;
 
   var markers = locations.map(function(location, index) {
+    console.log(location.name);
     let marker = new google.maps.Marker({
-      position: {lat: parseFloat(location.venue.latitude), lng: parseFloat(location.venue.longitude)},
+      position: {lat: location.coordinates.latitude, lng: location.coordinates.longitude},
       map: map,
-      title: location.name.text,
+      title: location.name,
       icon: defaultIcon
     });
 
     marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfoWindow);
+      populateInfoWindow(this, largeInfoWindow, location);
       viewModel.currentLocationIndex(index);
     });
 
     marker.addListener('mouseover',function() {
       //this.setAnimation(google.maps.Animation.BOUNCE);
-      this.setIcon(defaultIcon);
+      this.setIcon(highlightedIcon);
     });
 
     marker.addListener('mouseout',function() {
@@ -85,8 +88,9 @@ function initMap(events) {
       this.setIcon(defaultIcon);
     });
     return {
-      title: location.venue.name,
-      marker: marker
+      title: location.name,
+      marker: marker,
+      business : location
     };
   });
   //Add markers to viewModel
@@ -94,44 +98,82 @@ function initMap(events) {
 
 
 }
-      // This function takes in a COLOR, and then creates a new marker
-      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-      // of 0, 0 and be anchored at 10, 34).
-      function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-          '|40|_|%E2%80%A2',
-          new google.maps.Size(21, 34),
-          new google.maps.Point(0, 0),
-          new google.maps.Point(10, 34),
-          new google.maps.Size(21,34));
-        return markerImage;
-      }
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
+}
 
-      function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-      // Clear the infowindow content to give the streetview time to load.
-      infowindow.setContent('');
-      infowindow.marker = marker;
+function serveRating(rating) {
+  //Based on rating number, serve image of stars;
+  const path = 'assets/yelp_stars/web_and_ios/regular/';
+  const ratingMap = {
+    0: 'regular_0',
+    1: 'regular_1',
+    1.5: 'regular_1_half',
+    2: 'regular_2',
+    2.5: 'regular_2_half',
+    3: 'regular_3',
+    3.5: 'regular_3_half',
+    4: 'regular_4',
+    4.5: 'regular_4_half',
+    5: 'regular_5'
+  };
 
-      infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-      // Open the infowindow on the correct marker.
-      infowindow.open(map, marker);
-      map.setZoom(15);
-      map.setCenter(marker.getPosition());
-      // Make sure the marker property is cleared if the infowindow is closed.
+  return `assets/yelp_stars/web_and_ios/regular/${ratingMap[rating]}.png`;
+}
 
-    }
+function populateInfoWindow(marker, infowindow, business) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+  // Clear the infowindow content to give the streetview time to load.
+  infowindow.setContent('');
+  infowindow.marker = marker;
+  console.log('rating: ' + business.rating);
+  //Content Info HTML??
+  function markup(title, business) {
+    return `
+    <div class="media">
+    <img class="d-flex mr-3 rounded" src="${business.image_url}" alt="Generic placeholder image" height="165" width="165">
+    <div class="media-body">
+    <h5 class="mt-0">${title}</h5>
+    <div class="mb-2"><img src="${serveRating(business.rating)}"><span class="pl-2 align-middle">${business.review_count} reviews</span></div>
+    <address class="mb-0">
+    ${business.location.display_address.map(addr => `${addr}<br>`).join('')}
+    Phone: ${business.display_phone}
+    </address>
+    <div>
+    <a class="mr-auto"href="${business.url}" target="blank"><img class="float-right" src="/assets/Yelp_trademark_RGB_outline.png" height="48"></a>
+    </div>
+    </div>
+    </div>
+    `;
   }
-  //main ViewModel, where the modal will pop up
-  var ViewModel = function() {
-    var self = this;
-    self.infoWindow;
-    self.locationList = ko.observableArray([]);
-    self.currentLocationIndex = ko.observable(null);
-    self.highlightedIcon = makeMarkerIcon('FFFF24');
-    self.defaultIcon = makeMarkerIcon('0091ff');
+  infowindow.setContent(markup(marker.title, business));
+  // Open the infowindow on the correct marker.
+  infowindow.open(map, marker);
+  map.setZoom(15);
+  map.setCenter(marker.getPosition());
+  // Make sure the marker property is cleared if the infowindow is closed.
+}
+}
+
+//main ViewModel, where the modal will pop up
+var ViewModel = function() {
+  var self = this;
+  self.infoWindow;
+  self.locationList = ko.observableArray([]);
+  self.currentLocationIndex = ko.observable(null);
+  self.highlightedIcon = makeMarkerIcon('FFFF24');
+  self.defaultIcon = makeMarkerIcon('0091ff');
   // Input box to filter objects
   self.query = ko.observable('');
   self.toggleVisibility = function(locationList) {
@@ -163,7 +205,7 @@ function initMap(events) {
   }, self);
 
   self.openInfoWindow = function(location) {
-    populateInfoWindow(location.marker, self.infoWindow);
+    populateInfoWindow(location.marker, self.infoWindow, location.business);
     //TODO: Fix the highlighting, maybe add a property to the list
     self.currentLocationIndex(self.locationList.indexOf(location));
     console.log(self.currentLocationIndex());
@@ -181,61 +223,12 @@ function initMap(events) {
   };
 };
 
-
-NPS_API = '75861333-AF8F-47CE-85F3-8FAB0209987D';
-NPS_BASE_URL = 'https://developer.nps.gov/api/v0';
-
-
-baseURL = 'https://www.eventbriteapi.com/v3/';
-var jqxhr = function(latLng) {
-
-  $.ajax( "example.json", function() {
-    console.log( "success" );
-  })
-  .done(function() {
-    console.log( "second success" );
-  })
-  .fail(function() {
-    console.log( "error" );
-  })
-  .always(function() {
-    console.log( "complete" );
-  });
-};
-
-// events = request('Sunnyvale');
-var request = function(address, callback) {
-  var events;
-  $.ajax({
-    url: "https://www.eventbriteapi.com/v3/events/search/",
-    method: "GET",
-    data: { 'token' : 'JLYZPYK6UIYN3SUGQNKB',
-    'location.within': '10mi',
-    'location.address': address,
-    'expand': 'venue' },
-    dataType: "json"
-  }).done(function( data ) {
-    events = data.events;
-    callback(events);
-  }).fail(function( jqXHR, textStatus ) {
-    alert( "Request failed: " + textStatus );
-    return null;
-  });
-}
-
 const yelpKey = {
   clientId: 'XkRViyzWBDNefgBRi3llMw',
   clientSecret: 'LxCJ6jrkXIdB6PQlO9fggqh9UOL0fRvfMuwcM7qkjwC1UMH1p5xunMd2vlBJdQzG'
 };
 
-
-function randomString(length, chars) {
-  var result = '';
-  for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;
-}
-
-var yelp_request_2 = function(searchTerm) {
+var yelp_request_2 = function(searchTerm, callback) {
   var access_token = 'Bearer Gyup0cuo2-tAVdoRRYU66NCrnedTuPzbW5KjHjUSbPw_kE7ox7v6Icfy7dSmjzcrvZY6M_tV3bt8_ealRE2mH95_Aojzagm-OBV7QOTE9sJbBL5V6ZyThtL_e2eeWXYx';
   let myHeaders = new Headers();
   myHeaders.append("Authorization", access_token);
@@ -247,7 +240,7 @@ var yelp_request_2 = function(searchTerm) {
   }).then((json) => {
     console.log(json);
   });
-
+  `
   $.ajax({
     url: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token',
     method: 'POST',
@@ -261,6 +254,7 @@ var yelp_request_2 = function(searchTerm) {
       console.log(data);
     }
   })
+  `
   $.ajax({
     url: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972',
     //data: params,
@@ -268,7 +262,15 @@ var yelp_request_2 = function(searchTerm) {
     method: 'GET',
     dataType: 'json',
     success: function(data) {
-      console.log(data);
+      console.log(data.businesses);
+
+      var location = data.businesses.map( (business) => {
+        return {coordinates: business.coordinates,
+          name: business.name,
+          image_url: business.image_url};
+        });
+      console.log(location);
+      callback(data.businesses);
     },
     error: function(jqxhr, text) {
       console.log(text);
@@ -277,8 +279,7 @@ var yelp_request_2 = function(searchTerm) {
 };
 //TODO
 /*
-get endpoint to show data
+do one ajax request to get info on business?
 get data and populate list + marker
-Have option to filter by text, filter
 Figure out what to show on infowindow
 */
