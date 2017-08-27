6bc696f7192b3a72601213fc6486dc5d72092f23
Suffian Hamzah <suffianhamzah@gmail.com>
2017-08-27 15:24:00 -0700

    Got the higlighting to work

--- app.js
+++ app.js
@@ -1,103 +1,83 @@
+/*global ko, $ (jquery), and google */
 
-// Model
-var locations = [
-{ name: 'Yellowstone National Park',
-state: 'WY',
-country: 'USA',
-lat: 44.427895,
-lng: -110.588379 },
-{ name: 'Rothrock State Forest',
-state: 'PA',
-country: 'USA',
-lat: 40.720585,
-lng: -77.826965 },
-{ name: 'Yosemite National Park',
-state: 'CA',
-country: 'USA',
-lat: 37.865101,
-lng: -119.538330}];
 
-const mapItem = function mapDataModel(data) {
-  this.name = data.name;
-  this.active = ko.observable(false);
-
-};
-
-var map, center, largeInfoWindow;
+'use strict';
+var center,map, largeInfoWindow;
 function initialize() {
   yelp_request_2('food', initMap);
 }
+
 function initMap(events) {
   var markers = [];
+
   var bounds = new google.maps.LatLngBounds();
-  //Instantiate the ViewModel in initMap, so we now have the scope of initMap!
-  var viewModel = new ViewModel();
-  ko.applyBindings(viewModel);
-  console.log(events[0])
-  events.forEach(function(location) {
-    bounds.extend( new google.maps.LatLng(location.coordinates.latitude, location.coordinates.longitude));
-  });
-
-  /*
-  locations.forEach(function(location) {
-    bounds.extend( new google.maps.LatLng(location.latitude, location.long));
-  });
-  */
-  center = bounds.getCenter();
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: center
-  });
-
-  var defaultIcon = makeMarkerIcon('0091ff');
-  var highlightedIcon = makeMarkerIcon('FFFF24');
-  var locations = events;
-  largeInfoWindow = new google.maps.InfoWindow({
-    maxWidth: 375
-  });
-  largeInfoWindow.addListener('closeclick', function() {
-    largeInfoWindow.marker = null;
-    map.setZoom(15);
-    map.setCenter(center);
-    viewModel.currentLocationIndex(null);
-  });
-
-  viewModel.infoWindow = largeInfoWindow;
-
-  var markers = locations.map(function(location, index) {
-    console.log(location.name);
-    let marker = new google.maps.Marker({
-      position: {lat: location.coordinates.latitude, lng: location.coordinates.longitude},
-      map: map,
-      title: location.name,
-      icon: defaultIcon
+    //Instantiate the ViewModel in initMap, so we now have the scope of initMap!
+    var viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+    console.log(events[0])
+    events.forEach(function(location) {
+      bounds.extend( new google.maps.LatLng(location.coordinates.latitude, location.coordinates.longitude));
     });
 
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfoWindow, location);
-      viewModel.currentLocationIndex(index);
+    /*
+    locations.forEach(function(location) {
+      bounds.extend( new google.maps.LatLng(location.latitude, location.long));
+    });
+    */
+    center = bounds.getCenter();
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: center
     });
 
-    marker.addListener('mouseover',function() {
-      //this.setAnimation(google.maps.Animation.BOUNCE);
-      this.setIcon(highlightedIcon);
+    var defaultIcon = makeMarkerIcon('0091ff');
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+    var locations = events;
+    largeInfoWindow = new google.maps.InfoWindow({
+      maxWidth: 375
+    });
+    largeInfoWindow.addListener('closeclick', function() {
+      largeInfoWindow.marker = null;
+      map.setZoom(15);
+      map.setCenter(center);
+      viewModel.currentLocationIndex(null);
     });
 
-    marker.addListener('mouseout',function() {
-      //this.setAnimation(null);
-      this.setIcon(defaultIcon);
+    viewModel.infoWindow = largeInfoWindow;
+
+    var markers = locations.map(function(location, index) {
+      console.log(location.name);
+      let marker = new google.maps.Marker({
+        position: {lat: location.coordinates.latitude, lng: location.coordinates.longitude},
+        map: map,
+        title: location.name,
+        icon: defaultIcon
+      });
+
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfoWindow, location);
+        console.log(location);
+        viewModel.currentLocationIndex(location);
+      });
+
+      marker.addListener('mouseover',function() {
+        //this.setAnimation(google.maps.Animation.BOUNCE);
+        this.setIcon(highlightedIcon);
+      });
+
+      marker.addListener('mouseout',function() {
+        //this.setAnimation(null);
+        this.setIcon(defaultIcon);
+      });
+      return {
+        title: location.name,
+        marker: marker,
+        business : location
+      };
     });
-    return {
-      title: location.name,
-      marker: marker,
-      business : location
-    };
-  });
-  //Add markers to viewModel
-  viewModel.locationList(markers);
-
-
-}
+    //Add markers to viewModel
+    viewModel.locationList(markers);
+  }
 // This function takes in a COLOR, and then creates a new marker
 // icon of that color. The icon will be 21 px wide by 34 high, have an origin
 // of 0, 0 and be anchored at 10, 34).
@@ -132,46 +112,46 @@ function serveRating(rating) {
 }
 
 function populateInfoWindow(marker, infowindow, business) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-  // Clear the infowindow content to give the streetview time to load.
-  infowindow.setContent('');
-  infowindow.marker = marker;
-  console.log('rating: ' + business.rating);
-  //Content Info HTML??
-  function markup(title, business) {
-    return `
-    <div class="media">
-    <img class="d-flex mr-3 rounded" src="${business.image_url}" alt="Generic placeholder image" height="165" width="165">
-    <div class="media-body">
-    <h5 class="mt-0">${title}</h5>
-    <div class="mb-2"><img src="${serveRating(business.rating)}"><span class="pl-2 align-middle">${business.review_count} reviews</span></div>
-    <address class="mb-0">
-    ${business.location.display_address.map(addr => `${addr}<br>`).join('')}
-    Phone: ${business.display_phone}
-    </address>
-    <div>
-    <a class="mr-auto"href="${business.url}" target="blank"><img class="float-right" src="/assets/Yelp_trademark_RGB_outline.png" height="48"></a>
-    </div>
-    </div>
-    </div>
-    `;
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      // Clear the infowindow content to give the streetview time to load.
+      infowindow.setContent('');
+      infowindow.marker = marker;
+      console.log('rating: ' + business.rating);
+      //Content Info HTML??
+      function markup(title, business) {
+        return `
+        <div class="media">
+        <img class="d-flex mr-3 rounded" src="${business.image_url}" alt="Generic placeholder image" height="165" width="165">
+        <div class="media-body">
+        <h5 class="mt-0">${title}</h5>
+        <div class="mb-2"><img src="${serveRating(business.rating)}"><span class="pl-2 align-middle">${business.review_count} reviews</span></div>
+        <address class="mb-0">
+        ${business.location.display_address.map(addr => `${addr}<br>`).join('')}
+        Phone: ${business.display_phone}
+        </address>
+        <div>
+        <a class="mr-auto"href="${business.url}" target="blank"><img class="float-right" src="/assets/Yelp_trademark_RGB_outline.png" height="48"></a>
+        </div>
+        </div>
+        </div>
+        `;
+      }
+      infowindow.setContent(markup(marker.title, business));
+      // Open the infowindow on the correct marker.
+      infowindow.open(map, marker);
+      map.setZoom(15);
+      map.setCenter(marker.getPosition());
+      // Make sure the marker property is cleared if the infowindow is closed.
+    }
   }
-  infowindow.setContent(markup(marker.title, business));
-  // Open the infowindow on the correct marker.
-  infowindow.open(map, marker);
-  map.setZoom(15);
-  map.setCenter(marker.getPosition());
-  // Make sure the marker property is cleared if the infowindow is closed.
-}
-}
 
 //main ViewModel, where the modal will pop up
 var ViewModel = function() {
   var self = this;
   self.infoWindow;
   self.locationList = ko.observableArray([]);
-  self.currentLocationIndex = ko.observable(null);
+  self.currentLocationIndex = ko.observable();
   self.highlightedIcon = makeMarkerIcon('FFFF24');
   self.defaultIcon = makeMarkerIcon('0091ff');
   // Input box to filter objects
@@ -207,7 +187,7 @@ var ViewModel = function() {
   self.openInfoWindow = function(location) {
     populateInfoWindow(location.marker, self.infoWindow, location.business);
     //TODO: Fix the highlighting, maybe add a property to the list
-    self.currentLocationIndex(self.locationList.indexOf(location));
+    self.currentLocationIndex(location.business);
     console.log(self.currentLocationIndex());
   };
   self.makeMarkerBounce = function(location) {
@@ -240,7 +220,7 @@ var yelp_request_2 = function(searchTerm, callback) {
   }).then((json) => {
     console.log(json);
   });
-  `
+  /*
   $.ajax({
     url: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token',
     method: 'POST',
@@ -254,7 +234,7 @@ var yelp_request_2 = function(searchTerm, callback) {
       console.log(data);
     }
   })
-  `
+  */
   $.ajax({
     url: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972',
     //data: params,
@@ -262,14 +242,14 @@ var yelp_request_2 = function(searchTerm, callback) {
     method: 'GET',
     dataType: 'json',
     success: function(data) {
-      console.log(data.businesses);
+
 
       var location = data.businesses.map( (business) => {
         return {coordinates: business.coordinates,
           name: business.name,
           image_url: business.image_url};
         });
-      console.log(location);
+
       callback(data.businesses);
     },
     error: function(jqxhr, text) {
@@ -277,9 +257,10 @@ var yelp_request_2 = function(searchTerm, callback) {
     }
   });
 };
+
+
 //TODO
 /*
 do one ajax request to get info on business?
 get data and populate list + marker
-Figure out what to show on infowindow
 */
