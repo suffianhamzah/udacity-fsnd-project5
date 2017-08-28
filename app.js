8be0551691cecddc8b76e92ef620f5fe354139da
Suffian Hamzah <suffianhamzah@gmail.com>
2017-08-27 23:17:40 -0700

    Done commenting app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-/*global ko, $ (jquery), and google */
+/*global ko, $(jquery), and google.maps */
 
 'use strict';
 var map;
+
 /**
 * @description Initializes location data from Yelp API, map and viewModel
-* @gets data from Yelp API, and then creates the Map
 */
 function initialize() {
   //separate service for
   yelpRequest('food', 'Sunnyvale', initMap);
 }
 
+/**
+* @description Alerts the browser when unable to load google maps API
+*/
 function mapsError() {
  var errorHtml = `
  <div class="alert alert-info" role="alert">
@@ -20,6 +23,10 @@ function mapsError() {
  $('#map').html(errorHtml);
 }
 
+/**
+* @description Initializes map on browser
+* @param {Arr(object)} locations - location data
+*/
 function initMap(locations) {
   var center;
   var bounds = new google.maps.LatLngBounds();
@@ -41,9 +48,14 @@ function initMap(locations) {
     viewModel.locationList(viewModel.createMarkers(locations));
     ko.applyBindings(viewModel);
   }
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
+
+/**
+* @description takes in a COLOR, and then creates a new marker
+* icon of that color. The icon will be 21 px wide by 34 high, have an origin
+* of 0, 0 and be anchored at 10, 34).
+* @param {string} markerColor - 6 hex string representing a color
+* @returns {object} markerImage
+*/
 function makeMarkerIcon(markerColor) {
   var markerImage = new google.maps.MarkerImage(
     'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
@@ -55,6 +67,11 @@ function makeMarkerIcon(markerColor) {
   return markerImage;
 }
 
+/**
+* @description takes in rating, and will provide a filepath to the yelp rating image
+* @param {string} rating - A number ranging from 1 - 5, increments in 0.5
+* @returns {string} filepath for the rating's image
+*/
 function serveRating(rating) {
   //Based on rating number, serve image of stars;
   const path = 'assets/yelp_stars/web_and_ios/regular/';
@@ -74,43 +91,51 @@ function serveRating(rating) {
   return `assets/yelp_stars/web_and_ios/regular/${ratingMap[rating]}.png`;
 }
 
+/**
+* @description populates the infowindow based on marker information and Yelp business data
+* @param {object} marker - A Google Maps Marker object
+* @param {object} infowindow - A reference to Google Maps InfoWindow object
+* @param {object} business - Object containing Yelp Business data
+* @returns {string} filepath for the rating's image
+*/
 function populateInfoWindow(marker, infowindow, business) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-      // Clear the infowindow content to give the streetview time to load.
-      infowindow.setContent('');
-      infowindow.marker = marker;
-      console.log('rating: ' + business.rating);
-      //Content Info HTML??
-      function markup(title, business) {
-        return `
-        <div class="media">
-        <img class="d-flex mr-3 rounded business-img" src="${business.image_url}" alt="Generic placeholder image">
-        <div class="media-body">
-        <h5 class="mt-0">${title}</h5>
-        <div class="mb-2"><img src="${serveRating(business.rating)}"><span class="pl-2 align-middle">${business.review_count} reviews</span></div>
-        <address class="mb-0">
-        ${business.location.display_address.map(addr => `${addr}<br>`).join('')}
-        Phone: ${business.display_phone}
-        </address>
-        <div>
-        <a class="mr-auto"href="${business.url}" target="blank"><img class="float-right" src="/assets/Yelp_trademark_RGB_outline.png" height="48"></a>
-        <p>${business.price}</p>
-        </div>
-        </div>
-        </div>
-        `;
-      }
-      infowindow.setContent(markup(marker.title, business));
-      // Open the infowindow on the correct marker.
-      infowindow.open(map, marker);
-      map.setZoom(15);
-      map.setCenter(marker.getPosition());
-      // Make sure the marker property is cleared if the infowindow is closed.
-    }
-  }
 
-//main ViewModel, where the modal will pop up
+  //Creates infowindow content
+  function markup(title, business) {
+    return `
+    <div class="media">
+    <img class="d-flex mr-3 rounded business-img" src="${business.image_url}" alt="Generic placeholder image">
+    <div class="media-body">
+    <h5 class="mt-0">${title}</h5>
+    <div class="mb-2"><img src="${serveRating(business.rating)}"><span class="pl-2 align-middle">${business.review_count} reviews</span></div>
+    <address class="mb-0">
+    ${business.location.display_address.map(addr => `${addr}<br>`).join('')}
+    Phone: ${business.display_phone}
+    </address>
+    <div>
+    <a class="mr-auto"href="${business.url}" target="blank"><img class="float-right" src="/assets/Yelp_trademark_RGB_outline.png" height="48"></a>
+    <p>${business.price}</p>
+    </div>
+    </div>
+    </div>
+    `;
+  }
+  if (infowindow.marker != marker) {
+    infowindow.setContent('');
+    infowindow.marker = marker;
+    infowindow.setContent(markup(marker.title, business));
+    // Open the infowindow on the correct marker.
+    infowindow.open(map, marker);
+    map.setZoom(15);
+    map.setCenter(marker.getPosition());
+    // Make sure the marker property is cleared if the infowindow is closed.
+  }
+}
+
+/**
+* @description A KO ViewModel object containing the logic for the list item, and
+* behaviors for markers and infowindow
+*/
 var ViewModel = function() {
   var self = this;
   self.center;
@@ -120,21 +145,23 @@ var ViewModel = function() {
   self.defaultIcon = makeMarkerIcon('0091ff');
   self.query = ko.observable('');
 
-  // infoWindow
   self.infoWindow = new google.maps.InfoWindow({
     maxWidth: 375
   });
+
   self.infoWindow.addListener('closeclick', function() {
     self.infoWindow.marker = null;
     map.setZoom(14);
     map.setCenter(self.center);
     self.currentLocationIndex(null);
   });
-  //create marker function
-  // takes in an array of locations
+  /**
+  * @description Creates Google Maps Marker objects from location data
+  * @param {array(object)} locations - An array of location objects
+  */
   self.createMarkers = function(locations) {
     return locations.map(function(location) {
-      let marker = new google.maps.Marker({
+      var marker = new google.maps.Marker({
         position: {lat: location.coordinates.latitude, lng: location.coordinates.longitude},
         map: map,
         title: location.name,
@@ -164,6 +191,7 @@ var ViewModel = function() {
     });
     //Add markers to viewModel
   };
+
   // with reference from http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
   self.filteredList = ko.computed(function() {
     var filter = self.query().toLowerCase();
@@ -190,37 +218,60 @@ var ViewModel = function() {
     }
   }, self);
 
+  /**
+  * @description Triggers the 'click' event for a Google Maps Marker
+  * @param {object} location - An location object
+  */
   self.openInfoWindow = function(location) {
     google.maps.event.trigger(location.marker,'click');
     self.currentLocationIndex(location.business);
   };
+
+  /**
+  * @description Triggers the 'mouseover' event for a Google Maps Marker
+  * @param {object} location - An location object
+  */
   self.makeMarkerBounce = function(location) {
     google.maps.event.trigger(location.marker, 'mouseover');
   };
 
+  /**
+  * @description Triggers the 'mouseout' event for a Google Maps Marker
+  * @param {object} location - An location object
+  */
   self.makeMarkerNotBounce = function(location) {
     google.maps.event.trigger(location.marker, 'mouseout');
     location.marker.setIcon(self.defaultIcon);
   };
 
+  /**
+  * @description A wrapper KO function for the serveRating function
+  * @param {string} rating - An location object
+  */
   self.serveRating= function(rating) {
     return serveRating(rating);
   };
 };
 
 
-const yelpKey = {
-  clientId: 'XkRViyzWBDNefgBRi3llMw',
-  clientSecret: 'LxCJ6jrkXIdB6PQlO9fggqh9UOL0fRvfMuwcM7qkjwC1UMH1p5xunMd2vlBJdQzG'
-};
 
 
+/**
+* @description AJAX request for getting the Yelp Access token, and then getting the location data * from Yelp API
+* @param {string} searchTerm - term to search for e.g. food, deli
+* @param {string} locationStr - Place of where to search from
+* @param {function} callback - Callback function once data is successfully obtained
+*/
 var yelpRequest = function(searchTerm, locationStr, callback) {
   var params = {
     term: searchTerm,
     location: locationStr
   };
 
+  /**
+  * @description AJAX GET request to Yelp to obtain data
+  * @param {string} accessToken - Yelp's access token for authenticating API request
+  */
   function getLocations(accessToken){
     console.log(accessToken);
     $.ajax({
